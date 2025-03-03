01bd4ba5e355d862f242ec5bbe01e7a2fc336bc1
SUMAN MAJI <118503667+suman-maji@users.noreply.github.com>
2025-03-03 18:31:47 +0530

    Update Course.jsx

--- src/Pages/Course.jsx
+++ src/Pages/Course.jsx
@@ -3,7 +3,7 @@ import Branch from '../Component/Cards/Branch';
 import Semester from '../Component/Cards/Semester';
 import { useNavigate, useParams } from 'react-router-dom';
 import cse_img from "../Assets/cse_img.jpg";
-import it_img from "../Assets/it_img.jpg";
+it_img from "../Assets/it_img.jpg";
 import ece_img from "../Assets/ece_img.jpg";
 import aiml_img from "../Assets/aiml_img.jpg";
 import me_img from "../Assets/mech_img.jpg";
@@ -52,7 +52,7 @@ const Course = () => {
             
             <div>
                 <h2 className='text-2xl sm:text-3xl font-semibold text-gray-300 mb-5'>Select Your Branch</h2>
-                <div className='grid grid-cols-3 gap-4 sm:gap-6'>
+                <div className='grid grid-cols-3 gap-6 sm:gap-8'>
                     {branchName.map((elem, index) => (
                         <div key={index} onClick={() => selectBranch(elem)} 
                              className={`cursor-pointer transition-transform transform hover:scale-110 ${elem.name === selectedBranch ? "opacity-50" : ""}`}>
@@ -78,4 +78,4 @@ const Course = () => {
     );
 };
 
-export default Course
+export default Course;
