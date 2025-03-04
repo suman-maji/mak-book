8e884760f03c8f9bcea2294d241d52db4ed2ad98
SUMAN MAJI <118503667+suman-maji@users.noreply.github.com>
2025-03-04 18:29:01 +0530

    Update Spinner.jsx

--- src/Component/Error/Spinner.jsx
+++ src/Component/Error/Spinner.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import './Spinner.css'
+import React from "react";
+import "./Spinner.css";
 
 const Spinner = () => {
   return (
-    <>
-      
-    
-    <div className="w-full min-h-screen flex justify-center items-center">
-    <span className="loader"></span>
-   </div>
+    <div className="w-full min-h-screen flex justify-center items-center bg-gradient-to-br from-gray-100 to-gray-300 dark:from-gray-900 dark:to-gray-800 transition-all">
+      <div className="flex flex-col items-center">
+        <div className="loader"></div>
+        <p className="mt-4 text-gray-600 dark:text-gray-300 text-lg font-medium">
+          Loading, please wait...
+        </p>
+      </div>
+    </div>
+  );
+};
 
-    </>
-  )
-}
-
-export default Spinner
+export default Spinner;
