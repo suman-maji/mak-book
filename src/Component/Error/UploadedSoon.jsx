5683de250a257af66af982cacbf8f5caf2c95974
SUMAN MAJI <118503667+suman-maji@users.noreply.github.com>
2025-03-04 18:32:04 +0530

    Update UploadedSoon.jsx

--- src/Component/Error/UploadedSoon.jsx
+++ src/Component/Error/UploadedSoon.jsx
@@ -1,26 +1,22 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const UploadedSoon = () => {
   return (
-    <div className="text-center w-full h-[40vh] flex flex-col items-center justify-center mx-auto px-2 lg:px-8 z-20">
-        <h2 className="text-xl font-extrabold  text-white sm:text-4xl">
-            <span className="block">
-              Uploading Soon!
-            </span>
-        </h2>
-        <p className="text-normal sm:text-xl mt-4 max-w-md mx-auto ">
-            This page is not available right now, but it will be soon. Stay tuned!
-        </p>
-        <div className='mt-5'>
-            <NavLink 
-            to={"/"}
-            className="bg-white text-black px-3 py-2 sm:px-5 sm:py-3  rounded-full font-semibold ">
-                <button className=''>Back to Home</button>
-            </NavLink>
-        </div>
+    <div className="w-full h-[50vh] flex flex-col items-center justify-center text-center px-4 lg:px-8 bg-gradient-to-br from-gray-100 to-gray-300 dark:from-gray-900 dark:to-gray-800 transition-all">
+      <h2 className="text-2xl sm:text-4xl font-extrabold text-gray-900 dark:text-white">
+        Uploading Soon!
+      </h2>
+      <p className="text-gray-700 dark:text-gray-300 mt-4 text-lg sm:text-xl max-w-lg mx-auto">
+        This page is not available right now, but it will be soon. Stay tuned!
+      </p>
+      <NavLink to="/" className="mt-6">
+        <button className="bg-black dark:bg-white text-white dark:text-black px-5 py-3 sm:px-6 sm:py-3 rounded-full font-semibold shadow-md hover:scale-105 transition-transform duration-300">
+          Back to Home
+        </button>
+      </NavLink>
     </div>
-  )
-}
+  );
+};
 
-export default UploadedSoon
+export default UploadedSoon;
