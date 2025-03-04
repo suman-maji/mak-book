f6053a426f60007cd7f47eb235918f61761ed0d8
SUMAN MAJI <118503667+suman-maji@users.noreply.github.com>
2025-03-04 18:16:39 +0530

    Update PyqCard.jsx

--- src/Component/Common/PyqCard.jsx
+++ src/Component/Common/PyqCard.jsx
@@ -1,28 +1,28 @@
-import React from 'react'
+import React from "react";
 
-const PyqCard = ({pyqData}) => {
-    const dataArray = pyqData.map(item => {
-        const year = parseInt(Object.keys(item)[0]);
-        const link = item[year];
-        return [year, link];
-    });
+const PyqCard = ({ pyqData }) => {
+  // Transforming pyqData into an array of [year, link]
+  const dataArray = pyqData.map((item) => {
+    const year = parseInt(Object.keys(item)[0]);
+    const link = item[year];
+    return [year, link];
+  });
 
   return (
-    <div className='flex flex-wrap gap-4'>
-       {
-        dataArray.map((pyq)=>{
-            return (
-                <a 
-                className='border-[1px] bg-indigo-950 hover:bg-transparent rounded-full px-6 py-2'
-                href={pyq[1]}>
-                    {pyq[0]}
-                </a>
-            )
-        })
-       }
-        
+    <div className="flex flex-wrap gap-4 justify-center">
+      {dataArray.map((pyq, index) => (
+        <a
+          key={index}
+          href={pyq[1]}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="relative px-8 py-3 text-lg font-semibold text-white bg-gradient-to-r from-indigo-600 to-purple-700 rounded-lg shadow-lg transition-all duration-300 hover:scale-105 hover:shadow-2xl"
+        >
+          {pyq[0]}
+        </a>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default PyqCard
+export default PyqCard;
