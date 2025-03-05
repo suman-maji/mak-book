cf4b26c2182a96f2ea09261aebbb1ba5b3eb39c9
SUMAN MAJI <118503667+suman-maji@users.noreply.github.com>
2025-03-05 10:14:23 +0530

    Update Layout.jsx

--- src/Component/Layout.jsx
+++ src/Component/Layout.jsx
@@ -1,30 +1,18 @@
-import React from "react";
-import Navbar from "./Footer/Navbar";
-import { Outlet } from "react-router-dom";
-import Footer from "./Footer/Footer";
+import React from 'react'
+import Navbar from './Footer/Navbar'
+import { Outlet } from 'react-router-dom'
+import Footer from './Footer/Footer'
 
 const Layout = () => {
   return (
-    <div className="relative min-h-screen w-full bg-gray-50 text-gray-900 overflow-hidden">
-      {/* Main Content Wrapper */}
-      <div className="relative flex flex-col min-h-screen w-full">
-        {/* Navbar */}
-        <header className="sticky top-0 z-50 w-full bg-gray-50 shadow-md">
-          <Navbar />
-        </header>
-
-        {/* Page Content */}
-        <main className="flex-grow w-full">
-          <Outlet />
-        </main>
-
-        {/* Footer */}
-        <footer className="w-full bg-gray-50 shadow-inner">
-          <Footer />
-        </footer>
+    <>
+      <div className='sticky top-0 z-50'>
+        <Navbar />
       </div>
-    </div>
-  );
-};
+      <Outlet />
+      <Footer />
+    </>
+  )
+}
 
-export default Layout;
+export default Layout
