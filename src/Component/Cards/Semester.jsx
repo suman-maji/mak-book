372ea082359d7ffb5e8b74f3d4f1b6d5578fc82c
SUMAN MAJI <118503667+suman-maji@users.noreply.github.com>
2025-03-05 10:30:02 +0530

    Update Semester.jsx

--- src/Component/Cards/Semester.jsx
+++ src/Component/Cards/Semester.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Semester = ({ind}) => {
   return (
-    <div className='px-6 py-6 sm:py-9 sm:px-9 text-xs sm:text-base bg-gray-600 rounded-3xl flex justify-center items-center border-[1px] hover:bg-transparent cursor-pointer'>
+    <div className='px-6 py-6 sm:py-9 sm:px-9 text-xs sm:text-base font-semibold tracking-wider bg-gradient-to-r from-blue-500 to-blue-700 text-white rounded-3xl flex justify-center items-center border border-white shadow-md hover:bg-white hover:text-blue-600 hover:border-blue-600 hover:shadow-lg transition-all duration-300 cursor-pointer'>
         {
             `SEMESTER-${ind+1}`
         }
