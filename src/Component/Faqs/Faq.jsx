c14faee66ac123f165b8cbc8a771e72c23a7e411
SUMAN MAJI <118503667+suman-maji@users.noreply.github.com>
2025-03-05 20:52:14 +0530

    Update Faq.jsx

--- src/Component/Faqs/Faq.jsx
+++ src/Component/Faqs/Faq.jsx
@@ -7,7 +7,7 @@ const Faq = ({ elem }) => {
     <div className="rounded-lg overflow-hidden shadow-md">
       {/* Question Bar with purple-indigo gradient */}
       <button 
-        className="w-full text-left px-6 py-4 bg-gradient-to-r from-purple-600 to-indigo-600 text-white font-semibold text-lg sm:text-xl focus:outline-none"
+        className="w-full text-left px-10 py-8 bg-gradient-to-r from-purple-600 to-indigo-600 text-white font-semibold text-lg sm:text-xl focus:outline-none"
         onClick={() => setOpen(!open)}
       >
         {elem.Ques}
