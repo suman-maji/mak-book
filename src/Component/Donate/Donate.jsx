0e5e9b93acbda68750c91e11dfa033b9a8b2aee1
SUMAN MAJI <118503667+suman-maji@users.noreply.github.com>
2025-08-03 08:42:18 +0530

    Update Donate.jsx

--- src/Component/Donate/Donate.jsx
+++ src/Component/Donate/Donate.jsx
@@ -10,16 +10,16 @@ const Donate = () => {
       <div className="absolute top-0 left-0 w-80 h-80 bg-indigo-700 rounded-full filter blur-3xl opacity-30"></div>
       <div className="absolute top-0 right-0 w-80 h-80 bg-indigo-700 rounded-full filter blur-3xl opacity-30"></div>
       <div className="absolute -bottom-10 left-20 w-80 h-80 bg-indigo-700 rounded-full filter blur-3xl opacity-30"></div>
-      
+
       {/* Content Section */}
       <div className='relative sm:w-[50%] p-4 md:p-6 text-center md:text-left'>
-        <h1 className='text-3xl sm:text-5xl font-semibold sm:my-6 leading-tight'>
+        <h1 className='font-[Playfair] text-4xl sm:text-5xl font-semibold sm:my-6 leading-tight'>
           Join Our Community & Make a Difference
         </h1>
         <p className='w-[95%] sm:w-[80%] my-4 text-lg font-light'>
           Help us expand our resources and create a vast library accessible to all students.
         </p>
-        
+
         {/* Donate Button */}
         <NavLink 
           to={'/donate'}
@@ -29,7 +29,7 @@ const Donate = () => {
           <MdKeyboardDoubleArrowRight size={24} />
         </NavLink>
       </div>
-      
+
       {/* Image Section */}
       <div className='hidden md:flex justify-center items-center w-[35%]'>
         <img 
