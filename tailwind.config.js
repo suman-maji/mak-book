90f938279daa991962af1849452a610dd088241d
SUMAN MAJI <118503667+suman-maji@users.noreply.github.com>
2025-08-07 10:51:04 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         rubik: ["Rubik", "sans-serif"],
         poppins: ["Poppins", "sans-serif"],
         playfair: ["Playfair Display", "serif"],
+        cormorant: ['Cormorant', 'serif'],
       },
     },
   },
